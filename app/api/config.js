8c6cda8e3cc18b33df0a89ab4951da7945bc13b9
Evdokiya01 <oevlokia953@gmail.com>
2024-05-26 17:21:23 +0300

    yguhijok

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://evdokiyafinaldz.nomoredomainswork.ru/' 
+export const BASE_URL = 'https://evdokiyafinaldz.nomoredomainswork.ru/api' 
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
